0d97a011829131355ba1aff2dc00a9002c90e072
charleschuang1993 <charleschuang1993@gmail.com>
2025-06-09 23:54:02 +0800

    Add initial implementation of iris classifier using random forest and MLflow

---
+++ src/train.R
@@ -0,0 +1,29 @@
+# src/train.R
+library(mlflow)
+library(datasets)
+library(randomForest)
+
+mlflow_set_tracking_uri("http://localhost:5000")
+
+# start MLflow experiment
+mlflow_start_run()
+
+# read the iris dataset
+data(iris)
+
+# train a random forest model
+model <- randomForest(Species ~ ., data = iris, ntree = 100)
+
+# predict and calculate accuracy
+acc <- sum(predict(model, iris) == iris$Species) / nrow(iris)
+
+# save the model
+saveRDS(model, "models/iris_rf_model.rds")
+
+# record parameters, metrics, and artifacts in MLflow
+mlflow_log_param("ntree", 100)
+mlflow_log_metric("accuracy", acc)
+mlflow_log_artifact("models/iris_rf_model.rds")
+
+# end the MLflow run
+mlflow_end_run()
