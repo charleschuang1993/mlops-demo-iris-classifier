91588c66df05df8b915ca6ee936b3df5906492d2
charleschuang1993 <charleschuang1993@gmail.com>
2025-06-10 12:36:30 +0800

    Add example curl command and prediction output to start script for Plumber API

--- src/start-plumber-api.R
+++ src/start-plumber-api.R
@@ -8,5 +8,11 @@ pr <- plumber::plumb('/Users/charleschuang/Desktop/atgenomix/ToolDev/mlops-demo-
 # To disable this message, set a debug value.
 # This message is displayed once per session.
 
+# charleschuang@zhuangchengxundeMacBook-Pro mlops-demo-iris-classifier % curl -X POST http://localhost:8000/predict \
+#   -H "Content-Type: application/json" \
+#   -d '{"Sepal.Length": 5.1, "Sepal.Width": 3.5, "Petal.Length": 1.4, "Petal.Width": 0.2}'
+
+# {"predictions":["setosa"]}%   ## successful prediction
+
 # Swagger UI:
 # http://127.0.0.1:8000/__docs__/
