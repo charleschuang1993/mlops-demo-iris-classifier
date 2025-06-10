ac0ad573cb3c690358c101aba1b8207f84d329f9
charleschuang1993 <charleschuang1993@gmail.com>
2025-06-10 12:33:11 +0800

    Add comments to start script for Plumber API to clarify usage and error reporting

--- src/start-plumber-api.R
+++ src/start-plumber-api.R
@@ -1,1 +1,9 @@
 pr <- plumber::plumb('/Users/charleschuang/Desktop/atgenomix/ToolDev/mlops-demo-iris-classifier/src/plumber-api.R'); pr$run(host='0.0.0.0', port=8000)
+
+
+# bash: 
+# Running plumber API at http://0.0.0.0:8000
+# Running swagger Docs at http://127.0.0.1:8000/__docs__/
+# Error reporting has been turned off by default. See `pr_set_debug()` for more details.
+# To disable this message, set a debug value.
+# This message is displayed once per session.
